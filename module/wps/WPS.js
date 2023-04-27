1d43bb2b0c9502d9d7071416494394f619c21076
mikoycc <54666324+mikoycc@users.noreply.github.com>
2023-04-28 00:27:59 +0800

    Update WPS.js

--- module/wps/WPS.js
+++ module/wps/WPS.js
@@ -2,7 +2,7 @@
 
 [rewrite_local]
 
-^https:\/\/(vas|account)\.wps\.cn\/(query\/api\/v\d\/list_purchase_info|api\/users\/\d+\/overview) url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/WPSOffice.js
+^https:\/\/(vas|account)\.wps\.cn\/(query\/api\/v\d\/list_purchase_info|api\/users\/\d+\/overview) url script-response-body https://raw.githubusercontent.com/mikoycc/surge/main/module/wps/WPS.js
 
 [mitm] 
 
